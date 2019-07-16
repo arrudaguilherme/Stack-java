39a60d1526ef206dc0373d369932972b03c57923
Guilherme Arruda <guilhermearruda@Guilhermes-MacBook-Pro.local>
2019-07-15 17:48:52 -0700

    Stack app commit

---
+++ Stack.java
@@ -0,0 +1,40 @@
+package pilha;
+import stack.Stack; //imports the object Stack(class stack) from the same package
+
+public class Pilha<T> {
+    private Node <T> top;
+    
+    public boolean isEmpty(){
+        if(this.top==null){
+            return true;
+            
+        }else{
+            return false;
+        }
+    }    
+    
+    public boolean isFull(){
+        return false;
+        
+    }
+    
+    public void push(T valor){
+        Node <T> aux = new Node<T>(valor);
+        aux.setNext(this.top);
+        this.top=aux;
+        
+        
+    }
+    
+    public T top(){
+        return this.top.getInfo();
+    }    
+    
+    public T pop(){
+        Node <T> aux=this.top;
+        this.top=this.top.getNext();
+        return aux.getInfo();
+    }
+    
+    
+}
